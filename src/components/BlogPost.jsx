3f8a6df09c2d2621bde5f82b56c26b8e91dc92e2
Figby222 <figby222@outlook.com>
2024-10-12 03:07:30 -0700

    Add BlogPost Prop Types

--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -1,3 +1,19 @@
-const BlogPost = () => {};
+import PropTypes from "prop-types";
+
+const BlogPost = ({ useAllData, postId }) => {
+    return(
+        <>
+        </>
+    )
+};
+
+
+
+
+
+BlogPost.propTypes = {
+    useAllData: PropTypes.func.isRequired,
+    postId: PropTypes.number.isRequired
+}
 
 export default BlogPost;
