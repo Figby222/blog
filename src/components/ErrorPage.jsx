21f8664d212b5356ee2741fe01e446cd9fe03237
Figby222 <figby222@outlook.com>
2024-10-11 22:49:46 -0700

    Add Error Message Text to .main-heading

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -4,7 +4,7 @@ const ErrorPage = ({ status, message }) => {
     return (
         <>
             <h1 className="main-heading">
-                { status }
+                { status } - Error Message
             </h1>
         </>
     )
