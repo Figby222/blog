690865c339796bd028fdb96e289107aa29dd4338
Figby222 <figby222@outlook.com>
2024-10-11 22:28:28 -0700

    Add h1 with main-heading class

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -3,6 +3,9 @@ import PropTypes from "prop-types";
 const ErrorPage = ({ status, message }) => {
     return (
         <>
+            <h1 className="main-heading">
+                
+            </h1>
         </>
     )
 };
