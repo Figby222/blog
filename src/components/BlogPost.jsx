5dbad8105664ef581ab204bbf3efc788fa99263b
Figby222 <figby222@outlook.com>
2024-10-12 03:44:56 -0700

    Render .error heading

--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -4,6 +4,7 @@ const BlogPost = ({ useAllData, postId }) => {
     return(
         <>
             <h1 className="loading">Loading...</h1>
+            <h1 className="error">An error has occurred</h1>
             <main>
                 <h1 className="blog-title">Blog Title</h1>
                 <p className="blog-text">Blog Text</p>
