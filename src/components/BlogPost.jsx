4c366bc1d46223fc6965ca4055ffae6c257bf259
Figby222 <figby222@outlook.com>
2024-10-12 03:57:28 -0700

    Render title from data obj

--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -1,13 +1,15 @@
 import PropTypes from "prop-types";
 
 const BlogPost = ({ useAllData, postId }) => {
+    const { error, loading, data } = useAllData();
+    console.log(data);
+
     return(
         <>
             <h1 className="loading">Loading...</h1>
             <h1 className="error">An error has occurred</h1>
             <main>
-                <h1 className="blog-title">Blog Title</h1>
-                <p className="blog-text">Blog Text</p>
+                <h1 className="blog-title">{ data && data.title }</h1>
             </main>
         </>
     )
