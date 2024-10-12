5848351e180f47339bddbf463a79cfcb7569211f
Figby222 <figby222@outlook.com>
2024-10-11 23:11:26 -0700

    Add ErrorElement to root

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,9 +1,11 @@
 import App from "./App.jsx";
+import ErrorPage from "./components/ErrorPage.jsx";
 
 const routes = [
     {
         path: "/",
-        element: <App />
+        element: <App />,
+        errorElement: <ErrorPage />
     }
 ];
 
