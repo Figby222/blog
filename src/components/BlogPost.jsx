a41afddcfc8379842d8e657bd189c9372fabc3c6
Figby222 <figby222@outlook.com>
2024-10-12 03:40:12 -0700

    Add Blog Text text to blog text paragraph

--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -6,7 +6,7 @@ const BlogPost = ({ useAllData, postId }) => {
             <h1 className="loading">Loading...</h1>
             <main>
                 <h1 className="blog-title">Blog Title</h1>
-                <p className="blog-text"></p>
+                <p className="blog-text">Blog Text</p>
             </main>
         </>
     )
