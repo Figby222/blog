cda89eb973b4b831461c28cb23ce2a9a4d35891e
Figby222 <figby222@outlook.com>
2024-10-11 22:26:08 -0700

    Add ErrorPage Prop Types

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,5 +1,15 @@
 import PropTypes from "prop-types";
 
-const ErrorPage = () => {};
+const ErrorPage = ({ status, message }) => {
+    return (
+        <>
+        </>
+    )
+};
+
+ErrorPage.propTypes = {
+    status: PropTypes.number.isRequired,
+    message: PropTypes.string.isRequired,
+};
 
 export default ErrorPage;
