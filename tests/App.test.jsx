d7597f98571034cbc1d8f84d9689a581f0bc6418
Figby222 <figby222@outlook.com>
2024-10-11 21:00:12 -0700

    Add App.test.jsx

---
+++ tests/App.test.jsx
@@ -0,0 +1,11 @@
+import { describe, it, expect } from "vitest";
+
+describe("Stuff", () => {
+    it("true to be true", () => {
+        expect(true).toBe(true);
+    });
+    
+    it("False to be false", () => {
+        expect(false).toBe(false);
+    });
+})
