245bc2274b02185dd2675c2dc707532840376ca9
Figby222 <figby222@outlook.com>
2024-10-12 02:43:20 -0700

    Add BlogPost.text.jsx

---
+++ tests/BlogPost.test.jsx
@@ -0,0 +1,18 @@
+import { describe, it, expect } from "vitest";
+import { screen, render as _render } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import BlogPost from "../src/components/BlogPost.jsx";
+
+const render = (component) => {
+    return _render(<MemoryRouter>
+        { component }
+    </MemoryRouter>)
+}
+
+
+
+describe("BlogPost", () => {
+    it("Exists", () => {
+        expect(BlogPost).toBeDefined();
+    })
+})
