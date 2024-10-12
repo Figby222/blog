5e2fccb8708b008dd0a39674dd31e4a698860fa3
Figby222 <figby222@outlook.com>
2024-10-11 22:13:32 -0700

    Add ErrorPage.test.jsx

---
+++ tests/ErrorPage.test.jsx
@@ -0,0 +1,10 @@
+import { describe, it, expect } from "vitest";
+import { screen, render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import ErrorPage from "../src/components/ErrorPage.jsx";
+
+describe("Error Page", () => {
+    it("Exists", () => {
+        expect(ErrorPage).toBeDefined();
+    })
+})
