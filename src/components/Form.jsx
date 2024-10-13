ecd9cecd796053560b02b8e2de33d37256f092d6
Figby222 <figby222@outlook.com>
2024-10-12 23:21:02 -0700

    Make submit button text Submit

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,7 +4,7 @@ const Form = ({ submitListener, submitButtonText, children }) => {
     return (
         <>
             <form>
-                <button type="submit"></button>
+                <button type="submit">Submit</button>
             </form>
         </>
     )
