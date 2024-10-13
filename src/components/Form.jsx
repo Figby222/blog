8a26541931d593afc7248d2447858b20a182d46c
Figby222 <figby222@outlook.com>
2024-10-12 23:13:49 -0700

    Add Form Prop Types

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,3 +1,16 @@
-const Form = () => {};
+import PropTypes from "prop-types";
+
+const Form = ({ submitListener, submitButtonText, children }) => {
+    return (
+        <>
+        </>
+    )
+};
+
+Form.propTypes = {
+    submitListener: PropTypes.func.isRequired,
+    submitButtonText: PropTypes.string.isRequired,
+    children: PropTypes.any
+}
 
 export default Form;
