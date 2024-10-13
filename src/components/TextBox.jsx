287b732a81b6977c868279286917c2860a2bf6aa
Figby222 <figby222@outlook.com>
2024-10-13 05:41:45 -0700

    Add TextBox Prop Types

--- src/components/TextBox.jsx
+++ src/components/TextBox.jsx
@@ -1,3 +1,17 @@
-const TextBox = () => {};
+import PropTypes from "prop-types";
+
+const TextBox = ({ label, placeholder, value, onChange }) => {
+    return (
+        <>
+        </>
+    )
+};
+
+TextBox.propTypes = {
+    label: PropTypes.string.isRequired,
+    placeholder: PropTypes.string,
+    value: PropTypes.string,
+    onChange: PropTypes.func.isRequired,
+}
 
 export default TextBox;
