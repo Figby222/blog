75c309d48252d58e3709f208add4119c28781c93
Figby222 <figby222@outlook.com>
2024-10-12 21:38:06 -0700

    Add Comments.test.jsx

---
+++ tests/Comments.test.jsx
@@ -0,0 +1,14 @@
+import { describe, it, expect } from "vitest";
+import { screen } from "@testing-library/react";
+import { render } from "../lib/testing-utils";
+import Comment from "../src/components/Comment.jsx"
+
+
+
+
+
+describe("Comment", () => {
+    it("Exists", () => {
+        expect(Comment).toBeDefined();
+    })
+})
