61fb422aafe3949776bf7ee5b9f52409fc7957af
Figby222 <figby222@outlook.com>
2024-10-14 11:30:22 -0700

    Send loading heading only if api req is loading
    Send loading heading only if api request
    is loading

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -3,9 +3,12 @@ import PropTypes from "prop-types";
 const Blogs = ({ useAllData }) => {
     const { error, loading, data } = useAllData();
 
+    if (loading) {
+        return <h1 className="loading">Loading...</h1>
+    }
+
     return (
         <>
-            <h1 className="loading">Loading...</h1>
         </>
     )
 };
