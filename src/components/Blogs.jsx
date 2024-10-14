5067bcff466ca1bb1fe2b6fb59cc89bc1e2581c5
Figby222 <figby222@outlook.com>
2024-10-14 11:10:28 -0700

    Add Blogs Prop Types

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,3 +1,14 @@
-const Blogs = () => {};
+import PropTypes from "prop-types";
+
+const Blogs = ({ useAllData }) => {
+    return (
+        <>
+        </>
+    )
+};
+
+Blogs.PropTypes = {
+    useAllData: PropTypes.func.isRequired
+}
 
 export default Blogs;
