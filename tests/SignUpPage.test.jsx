f9533bb08e88aaf796d0777a537a37fbcc1db15b
Figby222 <figby222@outlook.com>
2024-10-15 09:33:56 -0700

    Make sure createAnAccount() is called with valid inputs

--- tests/SignUpPage.test.jsx
+++ tests/SignUpPage.test.jsx
@@ -1,7 +1,8 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, vi } from "vitest";
 import { screen } from "@testing-library/react";
 import { render } from "../lib/testing-utils.jsx";
 import SignUpPage from "../src/components/SignUpPage.jsx";
+import userEvent from "@testing-library/user-event";
 
 describe("Sign-Up page existence", () => {
     it("Exists", () => {
@@ -60,5 +61,28 @@ describe("Submit Button", () => {
     it("Has Submit text", () => {
         render(<SignUpPage createAnAccount={() => {}} />);
         expect(screen.queryByRole("button").textContent).toMatch(/Submit/i);
+    })
+})
+
+describe("Submitting the form with valid credentials", () => {
+    it("Calls createAnAccount", async () => {
+        const onSubmit = vi.fn(() => {})
+        render(<SignUpPage createAnAccount={onSubmit} />);
+
+        const usernameInput = screen.queryByLabelText(/Username/i);
+        const passwordInput = screen.queryByLabelText("Password");
+        const confirmPasswordInput = screen.queryByLabelText(/Confirm Password/i);
+        const submitButton = screen.queryByRole("button", { name: /Submit/i });
+
+        const user = userEvent.setup();
+
+        await user.type(usernameInput, "testUsername");
+        await user.type(passwordInput, "testPassword4444");
+        await user.type(confirmPasswordInput, "testPassword4444");
+
+        await user.click(submitButton);
+
+        expect(onSubmit)
+            .toHaveBeenCalled();
     })
 })
