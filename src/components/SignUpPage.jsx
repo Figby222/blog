2d6967112ce3e1cb812354d0f88f70b42d81302d
Figby222 <figby222@outlook.com>
2024-10-15 10:28:50 -0700

    Call createAnAccount() with field states

--- src/components/SignUpPage.jsx
+++ src/components/SignUpPage.jsx
@@ -1,23 +1,32 @@
 import PropTypes from "prop-types";
 import Form from "./Form.jsx";
+import { useState } from "react";
 
 const SignUpPage = ({ createAnAccount }) => {
-
+    const [ username, setUsername ] = useState("");
+    const [ password, setPassword ] = useState("");
+    const [ confirmPassword, setConfirmPassword ] = useState("");
 
     return (
         <>
-            <Form submitListener={() => createAnAccount("testUsername", "testPassword4444", "testPassword4444")} submitButtonText={"Submit"}>
+            <Form submitListener={() => createAnAccount(username, password, confirmPassword)} submitButtonText={"Submit"}>
                 <label htmlFor="username" className="username">
                     Username
-                    <input type="text" name="username" id="username" />
+                    <input type="text" name="username" id="username" 
+                        value={username} onChange={(e) => setUsername(e.target.value)}
+                    />
                 </label>
                 <label htmlFor="password" className="password">
                     Password
-                    <input type="password" name="password" id="password" />
+                    <input type="password" name="password" id="password" 
+                        value={password} onChange={(e) => setPassword(e.target.value)}
+                    />
                 </label>
                 <label htmlFor="confirm-password" className="confirm-password">
                     Confirm Password
-                    <input type="password" name="confirm_password" id="confirm-password" />
+                    <input type="password" name="confirm_password" id="confirm-password" 
+                        value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}
+                    />
                 </label>
 
             </Form>
