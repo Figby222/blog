bd78c99009dadfb6b8e274ad391e1d63c53b3ac3
Figby222 <figby222@outlook.com>
2024-10-15 16:22:33 -0700

    Make sure Errors is defined

---
+++ tests/Errors.test.jsx
@@ -0,0 +1,10 @@
+import { describe, it, expect } from "vitest";
+import { screen } from "@testing-library/react";
+import { render } from "../lib/testing-utils.jsx";
+import Errors from "../src/components/Errors.jsx";
+
+describe("Errors existence", () => {
+    it("Exists", () => {
+        expect(Errors).toBeDefined();
+    })
+})
