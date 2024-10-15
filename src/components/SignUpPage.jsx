dc3f2cb4eb3b388c9783524726f754f97333f3a7
Figby222 <figby222@outlook.com>
2024-10-15 09:01:56 -0700

    Nest fields in Form

--- src/components/SignUpPage.jsx
+++ src/components/SignUpPage.jsx
@@ -1,20 +1,24 @@
 import PropTypes from "prop-types";
+import Form from "./Form.jsx";
 
 const SignUpPage = ({ createAnAccount }) => {
     return (
         <>
-            <label htmlFor="username" className="username">
-                Username
-                <input type="text" name="username" id="username" />
-            </label>
-            <label htmlFor="password" className="password">
-                Password
-                <input type="password" name="password" id="password" />
-            </label>
-            <label htmlFor="confirm-password" className="confirm-password">
-                Confirm Password
-                <input type="password" name="confirm_password" id="confirm-password" />
-            </label>
+            <Form submitListener={() => {}} submitButtonText={""}>
+                <label htmlFor="username" className="username">
+                    Username
+                    <input type="text" name="username" id="username" />
+                </label>
+                <label htmlFor="password" className="password">
+                    Password
+                    <input type="password" name="password" id="password" />
+                </label>
+                <label htmlFor="confirm-password" className="confirm-password">
+                    Confirm Password
+                    <input type="password" name="confirm_password" id="confirm-password" />
+                </label>
+                
+            </Form>
         </>
     )
 };
