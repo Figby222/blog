f626cfa99e71142a99931955c75cd399c680a7bb
Figby222 <figby222@outlook.com>
2024-10-15 07:56:44 -0700

    Add SignUpPage Prop Types

--- src/components/SignUpPage.jsx
+++ src/components/SignUpPage.jsx
@@ -1,3 +1,14 @@
-const SignUpPage = () => {};
+import PropTypes from "prop-types";
+
+const SignUpPage = ({ createAnAccount }) => {
+    return (
+        <>
+        </>
+    )
+};
+
+SignUpPage.propTypes = {
+    createAnAccount: PropTypes.func.isRequired,
+}
 
 export default SignUpPage;
