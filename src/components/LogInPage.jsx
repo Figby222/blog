07c137361aab001c5f8b0a6617bb364d2fbccfdd
Figby222 <figby222@outlook.com>
2024-10-17 00:07:31 -0700

    Nest fields in Form

--- src/components/LogInPage.jsx
+++ src/components/LogInPage.jsx
@@ -1,20 +1,22 @@
 import PropTypes from "prop-types";
+import Form from "./Form";
 
 const LogInPage = ({ logInUser }) => {
     return (
         <>
-            <label className="username" htmlFor="username">
-                Username
-                <input type="text" name="username" id="username" />
-            </label>
-            <label className="password" htmlFor="password">
-                Password
-                <input type="password" name="password" id="password" />
-            </label>
+            <Form submitListener={() => {}} submitButtonText={""}>
+                <label className="username" htmlFor="username">
+                    Username
+                    <input type="text" name="username" id="username" />
+                </label>
+                <label className="password" htmlFor="password">
+                    Password
+                    <input type="password" name="password" id="password" />
+                </label>
+            </Form>
         </>
     )
 };
-
 LogInPage.propTypes = {
     logInUser: PropTypes.func.isRequired
 };
