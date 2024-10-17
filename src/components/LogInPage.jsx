9259d195ed40b659914300fd6947c82b9f7e584d
Figby222 <figby222@outlook.com>
2024-10-17 00:43:18 -0700

    Call logInUser() with field states

--- src/components/LogInPage.jsx
+++ src/components/LogInPage.jsx
@@ -1,18 +1,27 @@
 import PropTypes from "prop-types";
 import Form from "./Form";
+import { useState } from "react";
 
 const LogInPage = ({ logInUser }) => {
+    const [ username, setUsername ] = useState("");
+    const [ password, setPassword ] = useState("");
 
     return (
         <>
-            <Form submitListener={() => logInUser("testValidUsername", "testValidPassword4444")} submitButtonText={"Submit"}>
+            <Form submitListener={() => logInUser(username, password)} submitButtonText={"Submit"}>
                 <label className="username" htmlFor="username">
                     Username
-                    <input type="text" name="username" id="username" />
+                    <input type="text" name="username" id="username" 
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
                 </label>
                 <label className="password" htmlFor="password">
                     Password
-                    <input type="password" name="password" id="password" />
+                    <input type="password" name="password" id="password" 
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
                 </label>
             </Form>
         </>
