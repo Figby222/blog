4919d16e28d727d9cd68aca492955dd940f7e4ce
Figby222 <figby222@outlook.com>
2024-10-16 23:33:42 -0700

    Add LogInPage Prop Types

--- src/components/LogInPage.jsx
+++ src/components/LogInPage.jsx
@@ -1,3 +1,14 @@
-const LogInPage = () => {};
+import PropTypes from "prop-types";
+
+const LogInPage = ({ logInUser }) => {
+    return (
+        <>
+        </>
+    )
+};
+
+LogInPage.propTypes = {
+    logInUser: PropTypes.func.isRequired
+};
 
 export default LogInPage;
