50321f7ae3e4bde02728f982313d4e8e67803313
Figby222 <figby222@outlook.com>
2024-10-21 01:29:00 -0700

    Add /posts/:postId route

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,18 @@
 import App from "./App.jsx";
 import ErrorPage from "./components/ErrorPage.jsx";
+import BlogPost from "./components/BlogPost.jsx";
+import { useBlogPostData, createComment } from "./utils/api.jsx";
+const postId = 4;
 
 const routes = [
     {
         path: "/",
         element: <App />,
         errorElement: <ErrorPage />
+    },
+    {
+        path: "/posts/:postId",
+        element: <BlogPost useAllData={useBlogPostData} postId={postId} createComment={createComment} />
     }
 ];
 
