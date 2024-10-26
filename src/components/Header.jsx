b3657da99c93432d50db60d9ceec7cfa08eb2c17
Figby222 <figby222@outlook.com>
2024-10-26 03:27:44 -0700

    Add Header Prop Types

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,24 @@
-const Header = () => {};
+import PropTypes from "prop-types";
+
+const Header = ({ links, loggedInUser }) => {
+    return (
+        <>
+        </>
+    )
+};
+
+Header.defaultProps = { loggedInUser: null }
+
+Header.propTypes = {
+    links: PropTypes.arrayOf(PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        path: PropTypes.string.isRequired,
+        isCurrentPage: PropTypes.bool.isRequired
+    })),
+    loggedInUser: PropTypes.shape({
+        username: PropTypes.string.isRequired,
+        userId: PropTypes.number.isRequired,
+    })
+}
 
 export default Header;
