21a4377e05a76790c55880e4b854d662c6b4129c
Figby222 <figby222@outlook.com>
2024-10-26 05:45:03 -0700

    Add main title h1

--- src/components/LogInPage.jsx
+++ src/components/LogInPage.jsx
@@ -22,6 +22,7 @@ const LogInPage = ({ logInUser, storeBearerToken }) => {
 
     return (
         <>
+            <h1 className="main-title"></h1>
             <Link>Blogs</Link>
             <Link>Sign Up</Link>
             <Link>Log In</Link>
