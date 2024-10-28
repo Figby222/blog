fa033f9210f9f434738592b0126f8d81b271d57f
Figby222 <figby222@outlook.com>
2024-10-28 03:22:40 -0700

    Set unauthorized error message if error status 403
    Set unauthorized error message if error status is
    403

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,11 +1,21 @@
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
 
 const ErrorPage = ({ status, message }) => {
+    const error = useRouteError();
+
+    let errorStatus = status;
+    let errorMessage = message;
+
+    if (error && error.status != 404) {
+        errorStatus = error.status;
+        errorMessage = "You are not authorized to view this resource";
+    }
+    
     return (
         <>
             <h1 className="main-heading">
-                { status } - { message }
+                { error ? 403 : status } - { errorMessage }
             </h1>
             <Link className="home-link" to="/">
                 Home
